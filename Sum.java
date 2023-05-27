dc45ada4222a76dbd07280112afec531e43bb983
Ritik Patel <ritikpatel82993@gmail.com>
2023-05-27 18:54:33 +0530

    added message in Sum.java

--- Sum.java
+++ Sum.java
@@ -2,6 +2,6 @@ public class Sum{
     public static void main(String[] args) {
         int a = 15;
         int b= 15;
-        System.out.println("Hello World");
+        System.out.println(a+b);
     }
 }
